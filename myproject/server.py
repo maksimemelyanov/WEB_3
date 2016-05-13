11c5be830e9154a0923b8e8706be67c726794bef
maksimemelyanov <maksim.i.emelyanov@gmail.com>
2016-05-13 20:46:34 +0500

    Update for fifth HW

--- myproject/server.py
+++ myproject/server.py
@@ -1,10 +1,49 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import pyramid.httpexceptions as exc
 import os
 
-HOST = "localhost"
+from pyramid.wsgi import wsgiapp
+
+HOST = '0.0.0.0'
 PORT = 8000
 
 TOP = "<div class='top'>Middleware TOP</div>"
 BOTTOM = "<div class='botton'>Middleware BOTTOM</div>"
+
+def index_html(environ, start_response):
+    file = open('./index.html', 'r')
+	data = file.read()
+	file.close()
+	
+	return Response(data)
+
+def aboutme_html(environ, start_response):
+    file = open('./about/aboutme.html', 'r')
+	data = file.read()
+	file.close()
+	
+	return Response(data)
+
+
+MIDDLEWARE_TOP = "<div class='top'>Middleware TOP</div>"
+MIDDLEWARE_BOTTOM =  "<div class='botton'>Middleware BOTTOM</div>"
+
+
+if __name__ == '__main__':
+    config = Configurator()
+    config.add_route('ind1', '/')
+    config.add_route('ind2', '/index.html')
+    config.add_route('about', '/about/aboutme.html')
+	config.add_view(index_html, route_name='ind1')
+	config.add_view(index_html, route_name='ind2')
+    config.add_view(aboutme_html, route_name='about')
+    app = config.make_wsgi_app()
+    MWApp = MiddleWare(app)
+    server = make_server(HOST, PORT, MWApp)
+    server.serve_forever()
+
 
 class MiddleWare(object):
     def __init__(self, app):
@@ -19,22 +58,4 @@ class MiddleWare(object):
         return page
 
 
-def app(environ, start_response):
 
-    res = environ['PATH_INFO']
-    path = './' + res
-
-    if not os.path.isfile(path):
-        path ='./index.html'
-
-    fd = open(path,'r')
-    fileContent = fd.read()
-    fd.close()
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return fileContent
-
-app = MiddleWare(app)
-
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host=HOST, port=PORT)
